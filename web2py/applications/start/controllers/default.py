36ed864fa291fbcdf3d7566590892a85a8fab9c1
kevinjacobberger <kjacobbe@ucsc.edu>
2015-12-07 21:17:59 -0800

    voting system w/ debugging

--- web2py/applications/start/controllers/default.py
+++ web2py/applications/start/controllers/default.py
@@ -8,21 +8,127 @@
 ## - download is for downloading files uploaded in the db (does streaming)
 #########################################################################
 
+from gluon import utils as gluon_utils
+import json
 
 
 # @banana
 def index():
-    """
-    example action using the internationalization operator T and flash
-    rendered by views/default/index.html or views/generic.html
+    draft_id = gluon_utils.web2py_uuid()
+    """Fetch the Game of the Year Category as the default active Category"""
+    rows = db(db.cats.cat_name == "Game of the Year").select(db.cats.ALL)
+    first_cat = rows.first()
+    active_cat_id = first_cat.cat_id
+    active_cat_name = first_cat.cat_name
+    return dict(draft_id=draft_id, active_cat_id=active_cat_id, active_cat_name=active_cat_name)
 
-    if you need a simple wiki simply replace the two lines below with:
-    return auth.wiki()
-    """
-    logger.info("Here we are, in the controller.")
-    response.flash = T("Hello World")
-    return dict(message=T('Welcome to web2py!'))
+def catsList():
+    draft_id = gluon_utils.web2py_uuid()
+    user_id = auth.user_id
+    return dict(draft_id=draft_id, user_id=user_id)
 
+def show_cat():
+    draft_id = gluon_utils.web2py_uuid()
+    cat_id = request.args(0)
+    rows = db(db.cats.cat_id == cat_id).select(db.cats.ALL)
+    first_cat = rows.first()
+    cat_name = first_cat.cat_name
+    user_id = auth.user_id
+    response.flash = T(cat_name)
+    return dict(draft_id=draft_id, cat_name=cat_name, cat_id=cat_id, user_id=user_id)
+
+def load_cats():
+    """Loads all Categories"""
+    rows = db().select(db.cats.ALL)
+    d = {r.cat_id: {'cat_name': r.cat_name,
+                        'is_editing': r.is_editing,
+                        }
+         for r in rows}
+    return response.json(dict(cat_dict=d))
+
+def load_discs():
+    """Loads the correct discs within each cat"""
+    discs_list = db(db.discs.cat_loc == request.vars.cat_id).select(db.discs.ALL)
+    d = {r.disc_id: {'disc_name': r.disc_name,
+                        'is_editing': r.is_editing,
+                        'cat_loc': r.cat_loc,
+                        'author': r.author,
+                        }
+         for r in discs_list}
+    response.flash = T("Discs Loaded")
+    return response.json(dict(disc_dict=d))
+
+
+def load_games():
+    """Loads all Categories"""
+    rows = db(db.games.cat_loc == request.vars.cat_id).select(db.games.ALL)
+    g = {r.game_id: {'title': r.title,
+                        'game_id': r.game_id,
+                        'votes': r.votes
+                        }
+         for r in rows}
+    return response.json(dict(game_dict=g))
+
+@auth.requires_signature()
+def add_cat():
+    db.cats.update_or_insert((db.cats.cat_id == request.vars.cat_id),
+            author=request.vars.user_id,
+            cat_id=request.vars.cat_id,
+            cat_name=request.vars.cat,
+            is_editing=request.vars.is_editing)
+    return "ok"
+
+@auth.requires_signature()
+def add_disc():
+    db.discs.update_or_insert((db.discs.disc_id == request.vars.disc_id),
+            author=request.vars.user_id,
+            disc_id=request.vars.disc_id,
+            disc_name=request.vars.disc,
+            cat_loc=request.vars.cat_loc,
+            is_editing=request.vars.is_editing)
+    return "ok"
+
+
+def add_game():
+    form = SQLFORM(db.games)
+    form.vars.cat_loc = request.args(0)
+    form.vars.game_id = request.args(1)
+    form.vars.votes = 0
+    if form.process().accepted:
+        session.flash = T("data inserted")
+        redirect(URL('index'))
+    return dict(form=form)
+
+def cast_vote():
+    db.discs.update_or_insert((db.games.game_id == request.vars.game_id),
+            votes=request.vars.votes)
+    return "ok"
+
+@auth.requires_signature()
+def edit_cat():
+    db.cats.update_or_insert((db.cats.cat_id == request.vars.cat_id),
+            is_editing=request.vars.is_editing)
+    return "ok"
+
+@auth.requires_signature()
+def edit_disc():
+    db.discs.update_or_insert((db.discs.disc_id == request.vars.disc_id),
+            is_editing=request.vars.is_editing)
+    return "ok"
+
+@auth.requires_signature()
+def delete_cat():
+    cat_to_delete = request.vars.cat_id
+    """delete the selected Category"""
+    db(db.cats.cat_id == cat_to_delete).delete()
+    return "ok"
+
+@auth.requires_signature()
+def delete_disc():
+    disc_to_delete = request.vars.disc_id
+    """delete the selected Discussion"""
+    db(db.discs.disc_id == disc_to_delete).delete()
+    return "ok"
 
 def user():
     """
@@ -42,6 +148,11 @@ def user():
     """
     return dict(form=auth())
 
+def reset():
+    db(db.games.id > 0).delete()
+    session.flash = T("Database has been reset")
+    redirect(URL('default', 'index'))
+    return
 
 @cache.action()
 def download():
@@ -60,5 +171,3 @@ def call():
     supports xml, json, xmlrpc, jsonrpc, amfrpc, rss, csv
     """
     return service()
-
-
--- web2py/applications/start/models/tables.py
+++ web2py/applications/start/models/tables.py
@@ -14,3 +14,41 @@
 ## >>> rows=db(db.mytable.myfield=='value').select(db.mytable.ALL)
 ## >>> for row in rows: print row.id, row.myfield
 #########################################################################
+
+from datetime import datetime
+
+db.define_table('cats',
+             Field('author', db.auth_user, default=auth.user_id),
+             Field('cat_name', 'text'),
+             Field('cat_id'), # To uniquely identify categories.
+             Field('is_editing', 'boolean', default=False),
+             Field('cat_time', 'datetime', default=datetime.utcnow()),
+             Field('front_runner', 'text'),
+            )
+
+db.define_table('discs',
+             Field('author', db.auth_user, default=auth.user_id),
+             Field('disc_name', 'text'),
+             Field('disc_id'), # To uniquely identify categories.
+             Field('is_editing', 'boolean', default=False),
+             Field('disc_time', 'datetime', default=datetime.utcnow()),
+             Field('front_runner', 'text'),
+             Field('cat_loc'), # Reference to the category
+            )
+
+db.define_table('games',
+            Field('title'),
+            Field('game_id'),
+            Field('cat_loc'),
+            Field('votes','integer',default = 0),
+            )
+
+db.define_table('we',
+            Field('geee','integer',default = 0),
+            )
+db.games.game_id.writable=False
+db.games.game_id.readable=False
+db.games.cat_loc.writable=False
+db.games.cat_loc.readable=False
+db.games.votes.writable=False
+db.games.votes.readable=False
